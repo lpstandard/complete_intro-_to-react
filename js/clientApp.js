bba35065c155c1cfe9c74b522714798d7584f210
Tony Kung <lps.tony@gmail.com>
2016-10-16 22:01:26 -0500

    introduction

---
+++ js/clientApp.js
@@ -0,0 +1,25 @@
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title) 
+      )    
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
+
+var MyFirstComponent = (
+  div(null, 
+    MyTitleFact({title: 'Props are great',  color: 'red'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'));
