bb2d7a113b4272c65c3f9d89263ecbca1b382a00
Tony Kung <lps.tony@gmail.com>
2016-10-25 18:09:32 -0500

    create MyTitle.js

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,25 +1,16 @@
-
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
-var h1 = React.DOM.h1
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title) 
-      )    
-    )
-  }
-})
-
 var MyTitleFact = React.createFactory(MyTitle)
 var ce = React.createElement
 
 var MyFirstComponent = (
-  div(null, 
-    MyTitleFact({title: 'Props are great',  color: 'red'}),
+  div(null,
+    MyTitleFact({title: 'Props are great', color: 'red'}),
     React.createElement(MyTitle, {title: 'Use props everywhere', color: 'mediumaquamarine'}),
     ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
   )
 )
 
-ReactDOM.render(MyFirstComponent, document.getElementById('app'));
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
