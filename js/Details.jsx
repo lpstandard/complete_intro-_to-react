f2d376cdca64e1914f180a912b8cd56664319f46
Tony Kung <lps.tony@gmail.com>
2016-12-30 09:52:28 -0600

    FEM step 24

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,10 +1,15 @@
 import React, { PropTypes } from 'react'
 const Header = require('./Header')
+const { connector } = require('./Store')
 
 class Details extends React.Component {
+  assignShow (id) {
+    const showArray = this.props.shows.filter((show) => show.imdbID === id)
+    return showArray[0]
+  },
   render () {
-    const params = this.props.params || {}
-    const { title, description, year, poster, trailer } = params
+    const { title, description, year, poster, trailer } = this.props.shows[
+      this.props.params.id]
     return (
       <div className='container'>
         <Header />
@@ -24,8 +29,10 @@ class Details extends React.Component {
   }
 }
 
+const { arrayOf, object } = React.PropTypes
+
 Details.propTypes = {
-  params: PropTypes.object
+  shows: arrayOf(object).isRequired
 }
 
-module.exports = Details
+module.exports = connector(Details)
--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,38 +1,23 @@
 /* global React ReactDOM */
 
 const React = require('react')
-const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
-const { shows } = require('../public/data')
 const { store } = require('./store')
 const { Provider } = require('react-redux')
 
 const App = React.createClass({
-  assignShow (nextState, replace) {
-    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
-
-    console.log('nextState', nextState, 'showArray', showArray)
-
-    if (showArray.length < 1) {
-      return replace('/')
-    }
-
-    // const newState = {}
-    Object.assign(nextState.params, showArray[0])
-    return nextState
-  },
   render () {
     return (
       <Provider store={store}>
         <Router history={hashHistory}>
           <Route path='/' component={Layout}>
             <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} shows={shows} />
-            <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+            <Route path='/search' component={Search} />
+            <Route path='/details/:id' component={Details} />
           </Route>
         </Router>
       </Provider>
@@ -40,4 +25,4 @@ const App = React.createClass({
   }
 })
 
-ReactDOM.render(<App />, document.getElementById('app'))
+module.exports = App
